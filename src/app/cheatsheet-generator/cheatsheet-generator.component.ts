95e8b34a89982e5971ae869589bbbea14d52abb4
Rohit Kumar <rkumar8092378845@gmail.com>
2025-12-05 00:52:51 +0530

    feat: improved cheatsheet generator page

--- src/app/cheatsheet-generator/cheatsheet-generator.component.ts
+++ src/app/cheatsheet-generator/cheatsheet-generator.component.ts
@@ -1,14 +1,15 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CheatsheetGeneratorService} from "./service/cheatsheet-generator.service";
 import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {MatCard, MatCardContent} from "@angular/material/card";
 import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatProgressSpinner} from "@angular/material/progress-spinner";
 import {MatIcon} from "@angular/material/icon";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-cheatsheet-generator',
@@ -24,20 +25,46 @@ import {MatInput} from "@angular/material/input";
     MatError,
     MatIcon,
     MatButton,
-    MatInput
+    MatInput,
+    MatIconButton,
+    NgForOf
   ],
   templateUrl: './cheatsheet-generator.component.html',
   styleUrl: './cheatsheet-generator.component.css'
 })
-export class CheatsheetGeneratorComponent {
+export class CheatsheetGeneratorComponent implements OnInit {
 
   technology = '';
   cheatsheetHtml: SafeHtml | null = null;
   loading = false;
   error: string | null = null;
+  hasCheatsheet = false; // Add this flag
+  protected savedCheatsheets: string[] = []; // Initialize as empty array
 
-  constructor(private _cheatSheetGeneratorService: CheatsheetGeneratorService, private sanitizer: DomSanitizer
+  constructor(private _cheatSheetGeneratorService: CheatsheetGeneratorService, private sanitizer: DomSanitizer, private snackBar: MatSnackBar
   ) {
+  }
+
+  ngOnInit(): void {
+
+    this.refreshSavedCheatSheet();
+  }
+
+  private refreshSavedCheatSheet() {
+    this._cheatSheetGeneratorService.findAllTechnologies()
+      .subscribe({
+        next: value => {
+          this.savedCheatsheets = value
+        },
+        error: (error) => {
+          console.log(error);
+          this.error = 'Failed to get saved cheatsheets: ' + error.error;
+          this.snackBar.open('❌ Failed to get saved cheatsheets', 'Close', {
+            duration: 3000,
+            panelClass: ['mat-snackbar-error']
+          });
+        }
+      })
   }
 
   generateCheatsheet() {
@@ -50,17 +77,21 @@ export class CheatsheetGeneratorComponent {
     this.loading = true;
     this.error = null;
     this.cheatsheetHtml = null;
+    this.hasCheatsheet = false;
 
     this._cheatSheetGeneratorService.generateCheatSheet(this.technology).subscribe({
       next: (htmlContent) => {
         const processedHtml = this.removeLinks(htmlContent);
         this.cheatsheetHtml = this.sanitizer.bypassSecurityTrustHtml(processedHtml);
+        this.hasCheatsheet = true; // Set flag to true
         this.loading = false;
+        this.refreshSavedCheatSheet()
       },
       error: (error) => {
         console.log(error);
         this.error = 'Failed to generate cheatsheet: ' + error.error;
         this.loading = false;
+        this.hasCheatsheet = false;
       }
     });
   }
@@ -78,4 +109,41 @@ export class CheatsheetGeneratorComponent {
     return doc.documentElement.outerHTML;
   }
 
+  protected deleteCheatsheet(technology: string) {
+    this._cheatSheetGeneratorService.deleteCheatSheet(technology).subscribe({
+      next: value => {
+        this.snackBar.open('✅ Cheatsheet deleted successfully!', 'Close', {
+          duration: 3000,
+          panelClass: ['mat-snackbar-success']
+        });
+
+        if (technology === this.technology) {
+          this.toDefaults()
+        }
+
+        this.refreshSavedCheatSheet();
+      },
+      error: (err) => {
+        console.error('Failed to delete cheatsheet', err);
+        this.snackBar.open('❌ Failed to delete cheatsheet. Try again.', 'Close', {
+          duration: 3000,
+          panelClass: ['mat-snackbar-error']
+        });
+      }
+    })
+  }
+
+  protected getCheatSheet(sheet: string) {
+    this.technology = sheet;
+    this.generateCheatsheet()
+  }
+
+  private toDefaults() {
+
+      this.cheatsheetHtml = null
+      this.technology = ''
+      this.loading = false
+      this.error = null
+      this.hasCheatsheet = false
+  }
 }
--- src/app/cheatsheet-generator/service/cheatsheet-generator.service.ts
+++ src/app/cheatsheet-generator/service/cheatsheet-generator.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {environment} from "../../../environments/environment";
@@ -20,6 +20,15 @@ export class CheatsheetGeneratorService {
       params,
       responseType: 'text'
     });
+  }
+
+  findAllTechnologies(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.apiUrl}/technologies`);
+  }
+
+  deleteCheatSheet(technology: string): Observable<string> {
+    const params = new HttpParams().set('technology', technology);
+    return this.http.delete<string>(`${this.apiUrl}/technologies`, {params: params});
   }
 
 }
